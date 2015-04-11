482cbd6f0374de38c8874f61f378b0790ef5da00
mariocq <mariocq@gmail.com>
2015-04-11 15:31:45 -0600

    Sitio completo cambiar el config

---
+++ application/logs/log-2015-04-11.php
@@ -0,0 +1,99 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2015-04-11 23:30:32 --> Config Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Hooks Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> UTF-8 Support Enabled
+DEBUG - 2015-04-11 23:30:32 --> Utf8 Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> URI Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> No URI present. Default controller set.
+DEBUG - 2015-04-11 23:30:32 --> Router Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Output Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Security Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Input Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2015-04-11 23:30:32 --> Language Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Loader Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Helper loaded: url_helper
+DEBUG - 2015-04-11 23:30:32 --> Helper loaded: file_helper
+DEBUG - 2015-04-11 23:30:32 --> Helper loaded: form_helper
+DEBUG - 2015-04-11 23:30:32 --> CI_Session Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Encryption: Auto-configured driver 'openssl'.
+DEBUG - 2015-04-11 23:30:32 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+DEBUG - 2015-04-11 23:30:32 --> Encryption Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Database Driver Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Session: Regenerate ID
+DEBUG - 2015-04-11 23:30:32 --> CI_Session routines successfully run
+DEBUG - 2015-04-11 23:30:32 --> Controller Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Session has already been instantiated as 'session'. Second attempt aborted.
+DEBUG - 2015-04-11 23:30:32 --> Form Validation Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Helper loaded: date_helper
+DEBUG - 2015-04-11 23:30:32 --> Helper loaded: html_helper
+DEBUG - 2015-04-11 23:30:32 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:32 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:33 --> File loaded: C:\xampp\htdocs\geo\application\views\Inicio.php
+DEBUG - 2015-04-11 23:30:33 --> Final output sent to browser
+DEBUG - 2015-04-11 23:30:33 --> Total execution time: 1.0791
+DEBUG - 2015-04-11 23:30:41 --> Config Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Hooks Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> UTF-8 Support Enabled
+DEBUG - 2015-04-11 23:30:41 --> Utf8 Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> URI Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> No URI present. Default controller set.
+DEBUG - 2015-04-11 23:30:41 --> Router Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Output Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Security Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Input Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2015-04-11 23:30:41 --> Language Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Loader Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Helper loaded: url_helper
+DEBUG - 2015-04-11 23:30:41 --> Helper loaded: file_helper
+DEBUG - 2015-04-11 23:30:41 --> Helper loaded: form_helper
+DEBUG - 2015-04-11 23:30:41 --> CI_Session Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Encryption: Auto-configured driver 'openssl'.
+DEBUG - 2015-04-11 23:30:41 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+DEBUG - 2015-04-11 23:30:41 --> Encryption Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Database Driver Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> CI_Session routines successfully run
+DEBUG - 2015-04-11 23:30:41 --> Controller Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Session has already been instantiated as 'session'. Second attempt aborted.
+DEBUG - 2015-04-11 23:30:41 --> Form Validation Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Helper loaded: date_helper
+DEBUG - 2015-04-11 23:30:41 --> Helper loaded: html_helper
+DEBUG - 2015-04-11 23:30:41 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:41 --> File loaded: C:\xampp\htdocs\geo\application\views\Inicio.php
+DEBUG - 2015-04-11 23:30:41 --> Final output sent to browser
+DEBUG - 2015-04-11 23:30:41 --> Total execution time: 0.7220
+DEBUG - 2015-04-11 23:30:48 --> Config Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Hooks Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> UTF-8 Support Enabled
+DEBUG - 2015-04-11 23:30:48 --> Utf8 Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> URI Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> No URI present. Default controller set.
+DEBUG - 2015-04-11 23:30:48 --> Router Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Output Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Security Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Input Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2015-04-11 23:30:48 --> Language Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Loader Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Helper loaded: url_helper
+DEBUG - 2015-04-11 23:30:48 --> Helper loaded: file_helper
+DEBUG - 2015-04-11 23:30:48 --> Helper loaded: form_helper
+DEBUG - 2015-04-11 23:30:48 --> CI_Session Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Encryption: Auto-configured driver 'openssl'.
+DEBUG - 2015-04-11 23:30:48 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+DEBUG - 2015-04-11 23:30:48 --> Encryption Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Database Driver Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> CI_Session routines successfully run
+DEBUG - 2015-04-11 23:30:48 --> Controller Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Session has already been instantiated as 'session'. Second attempt aborted.
+DEBUG - 2015-04-11 23:30:48 --> Form Validation Class Initialized
+DEBUG - 2015-04-11 23:30:48 --> Helper loaded: date_helper
+DEBUG - 2015-04-11 23:30:49 --> Helper loaded: html_helper
+DEBUG - 2015-04-11 23:30:49 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:49 --> Model Class Initialized
+DEBUG - 2015-04-11 23:30:49 --> File loaded: C:\xampp\htdocs\geo\application\views\Inicio.php
+DEBUG - 2015-04-11 23:30:49 --> Final output sent to browser
+DEBUG - 2015-04-11 23:30:49 --> Total execution time: 0.8070
